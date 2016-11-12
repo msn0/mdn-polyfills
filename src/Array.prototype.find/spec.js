59da03705e2e1e3459f671837ae2759af38ebd04
Michał Jezierski <ambinanitelo@gmail.com>
2016-11-12 23:13:06 +0100

    Array.prototype.find spec

---
+++ src/Array.prototype.find/spec.js
@@ -0,0 +1,39 @@
+import test from 'ava';
+import find from './find';
+
+test('should find object', t => {
+  const inventory = [
+    { name: 'apples', quantity: 2 },
+    { name: 'bananas', quantity: 0 },
+    { name: 'cherries', quantity: 5 }
+  ];
+
+  t.deepEqual(find.call(inventory, findCherries), { name: 'cherries', quantity: 5 });
+});
+
+test('should find primes', t => {
+  t.deepEqual(find.call([4, 5, 8, 12], isPrime), 5);
+});
+
+test('should return undefined if not found', t => {
+  t.deepEqual(find.call([4, 6, 8, 12], isPrime), undefined);
+});
+
+test('only the first one should be found', t => {
+  t.deepEqual(find.call([4, 5, 8, 12, 17], isPrime), 5);
+});
+
+function findCherries(fruit) {
+  return fruit.name === 'cherries';
+}
+
+function isPrime(element) {
+  let start = 2;
+  while (start <= Math.sqrt(element)) {
+    if (element % start++ < 1) {
+      return false;
+    }
+  }
+  return element > 1;
+}
+
