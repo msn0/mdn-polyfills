02bb7caf4ccd7bc5fbcafd53a465ead05d164f32
Michał Jezierski <ambinanitelo@gmail.com>
2016-11-12 23:17:24 +0100

    Object.assign spec

--- src/Object.assign/spec.js
+++ src/Object.assign/spec.js
@@ -1,10 +1,19 @@
 import test from 'ava';
 import assign from './assign';
 
-test('should assign new properties to object', t => {
+test('should merge objects', t => {
   const foo = { foo: 'foo' };
   const bar = { bar: 'bar' };
 
   t.deepEqual(assign(foo, bar), { foo: 'foo', bar: 'bar' });
+});
+
+test('should clone object', t => {
+  const foo = { foo: 'foo' };
+
+  const bar = assign({}, foo);
+
+  t.not(foo, bar);
+  t.deepEqual(foo, bar);
 });
 
