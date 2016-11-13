f342c54e5c23eb9a37606eb242e71ebade7ad1b0
Michał Jezierski <ambinanitelo@gmail.com>
2016-11-13 22:59:33 +0100

    build fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const webpack = require('webpack');
-const path = require('path');
 
 module.exports = {
   entry: {
     "Object.assign": "./src/Object.assign/index.js",
     "Array.prototype.find": "./src/Array.prototype.find/index.js",
-    "Array.prototype.from": "./src/Array.prototype.from/index.js"
+    "Array.prototype.from": "./src/Array.prototype.from/index.js",
+    "Array.prototype.filter": "./src/Array.prototype.filter/index.js",
+    "Array.prototype.forEach": "./src/Array.prototype.forEach/index.js"
   },
   output: {
     filename: "[name].js"
@@ -14,14 +15,10 @@ module.exports = {
     new webpack.optimize.UglifyJsPlugin()
   ],
   module: {
-
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel'
-      }
-    ]
-
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
   }
 };
