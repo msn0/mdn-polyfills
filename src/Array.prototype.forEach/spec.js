f87f4c89a5cc6d99507a2c665adcae047f850ca7
Michał Jezierski <ambinanitelo@gmail.com>
2016-11-13 22:36:40 +0100

    Array.prototype.forEach spec

---
+++ src/Array.prototype.forEach/spec.js
@@ -0,0 +1,13 @@
+import test from 'ava';
+import forEach from './forEach';
+
+test('should iterate through elements', t => {
+  const array = ['b', 'a', 'r'];
+  const result = [];
+
+  forEach.call(array, element => {
+    result.push(element);
+  });
+
+  t.deepEqual(array, result);
+});
