7e6909e6f6c457c1c13fced5f85e640c59889e6b
Michał Jezierski <ambinanitelo@gmail.com>
2016-11-13 22:52:36 +0100

    Array.prototype.filter spec

---
+++ src/Array.prototype.filter/spec.js
@@ -0,0 +1,12 @@
+import test from 'ava';
+import filter from './filter';
+
+test('should filter out values', t => {
+  const filtered = filter.call([12, 5, 8, 130, 44], isBigEnough);
+
+  t.deepEqual(filtered, [12, 130, 44]);
+});
+
+function isBigEnough(value) {
+  return value >= 10;
+}
