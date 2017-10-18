8d0ba5af7e9351adcd685bfd615a31bf9cd9a8a5
Riccardo Causo <rixlabs@gmail.com>
2017-10-18 14:47:36 +0200

    implement padEnd

---
+++ src/String.prototype.padEnd/spec.js
@@ -0,0 +1,11 @@
+import test from 'ava';
+import padEnd from './padEnd';
+
+test('String.prototype.padEnd', t => {
+    const str = 'abc';
+
+    t.true(padEnd.call(str, 10) === 'abc       ');
+    t.true(padEnd.call(str, 10, "foo") === 'abcfoofoof');
+    t.true(padEnd.call(str, 6, "123465") === 'abc123');
+    t.true(padEnd.call(str, 1) === 'abc');
+});
